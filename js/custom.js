d17fe807fa51614332d9905fb1c26eb316a8d8df
mingyeon.github.io <jojo9608@naver.com>
2024-08-05 15:33:07 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -4,12 +4,12 @@ $(function () {
         $(this).next().toggle();
     });
 
-    $('#bgVideo').YTPlayer({
-        videoURL: "https://www.youtube.com/embed/krwiZ7q9xC8",
-        containment: '.main_movie',
-        showControls: false,
-        playOnlyIfVisible: true,
-    })
+    // $('#bgVideo').YTPlayer({
+    //     videoURL: "https://www.youtube.com/embed/krwiZ7q9xC8",
+    //     containment: '.main_movie',
+    //     showControls: false,
+    //     playOnlyIfVisible: true,
+    // })
 
     const mainVisualSlide = new Swiper('.mainVisualSlide', {
         loop: true,
@@ -30,4 +30,17 @@ $(function () {
             $('#header').removeClass('on')
         }
     });
+
+    $(window).on('resize', function () {
+        $('#header .gnb').removeClass('on');
+        $('#header .gnb>ul>li ul').removeAttr('style');
+    });
+
+    $('#header .gnb>ul>li>a').on('click', function (e) {
+        if (('#header .gnb').hasClass('on')) {
+            e.preventDefault();
+            $(this).next().stop().slideDown();
+            $(this).parent().siblings().find('ul').stop().slideUp();
+        }
+    })
 });
